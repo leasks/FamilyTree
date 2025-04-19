466f24bd35419b295e7370a704e9f164bf940733
leasks <64079770+leasks@users.noreply.github.com>
2025-04-19 17:13:59 +0100

    Various updates

---
+++ FamilyTree iOS/ResourceList.swift
@@ -0,0 +1,8 @@
+//
+//  ResourceList.swift
+//  FamilyTree iOS
+//
+//  Created by Stephen Leask on 01/06/2024.
+//
+
+import Foundation
---
+++ FamilyTree iOS/ResourceListItem.swift
@@ -0,0 +1,8 @@
+//
+//  ResourceListItem.swift
+//  FamilyTree iOS
+//
+//  Created by Stephen Leask on 19/04/2025.
+//
+
+import Foundation
